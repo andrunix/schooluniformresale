637be09390605ff9d21ac4a6c99f034f21202251
Andrew Pierce <andrunix@gmail.com>
2015-03-09 13:15:24 -0400

    Added categories for items and setup relationship.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309151038) do
+ActiveRecord::Schema.define(version: 20150309170744) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150309151038) do
     t.integer  "user_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "category_id"
   end
 
   add_index "items", ["user_id"], name: "index_items_on_user_id"
