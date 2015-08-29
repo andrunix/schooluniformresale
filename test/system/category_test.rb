b1fb18e0f3d3cf55f971c79f51893c328bb93fc5
Andrew Pierce <andrunix@gmail.com>
2015-08-29 11:37:49 -0400

    Finish up categories tests.

--- test/system/category_test.rb
+++ test/system/category_test.rb
@@ -4,7 +4,6 @@ describe "Category Management" do
   include SystemHelper
 
   let(:category){ Fabricate :category }
-  let(:role) { Fabricate :role, name: 'Admin' }
   let(:user)  { Fabricate(:user, 
     password: 'password', 
     password_confirmation: 'password', 
@@ -17,7 +16,6 @@ describe "Category Management" do
 
   before do
     stub_user_session(user)
-    # visit categories_path
     visit new_category_path
   end
 
@@ -37,86 +35,19 @@ describe "Category Management" do
       find_link("Back").click
       assert has_content? "Listing Categories"
     end
+    
+    it "allows categories to be edited" do
+      find_link("Edit").click
+      fill_in :category_name, with: "Pants, Shorts and Skirts"
+      click_button "Update Category"
+      assert has_content?("Category was successfully updated")
+    end
 
-    # describe "Managing a new idea" do
-
-    #   before do
-    #     find("#added-idea")
-    #     find(".go-to-idea").click
-    #   end
-
-    #   it "allows ideas to be edited" do
-    #     find(".edit-idea").click
-    #     find(".new_idea")
-    #     find("#edit-idea-name").set("Updated Idea Title")
-    #     find(".update-idea").click
-    #     title = find(".idea-title")
-    #     assert title.has_content? "Updated Idea Title"
-    #   end
-
-    #   it "displays a confirmation modal when attempting to delete an idea" do
-    #     find(".delete-idea").click
-    #     assert has_content? "Are you you sure you want to delete the idea"
-    #   end
-
-    #   it "does not remove an idea if the user cancels the deletion modal" do
-    #     find(".delete-idea").click
-    #     within "#delete-idea" do
-    #       find(".cancel-delete").click
-    #     end
-    #     assert has_content? "A New Idea"
-    #   end
-
-    #   it "deletes an idea" do
-    #     find(".delete-idea").click
-    #     within "#delete-idea" do
-    #       find(".confirm-delete").click
-    #     end
-    #     refute has_content? "A New Idea"
-    #   end
-
-    #   it "should automatically add an upvote from the idea creator" do
-    #     upvote = find(".upvote")
-    #     count = upvote["count"]
-    #     assert_equal "1", count
-    #   end
-
-    #   it "allows undoing idea upvote" do
-    #     upvote = find(".upvote")
-    #     upvote.click
-    #     sleep(1) # Not ideal - but needed to ensure for the count data-attribute to get updated
-    #     count = upvote["count"]
-    #     assert_equal "0", count
-    #   end
-
-    #   describe "Commenting on an Idea" do
-
-    #     before do
-    #       within "#add-comment" do
-    #         fill_in :comment_body, with: "Fantastic Idea!"
-    #         click_button "Comment"
-    #       end
-    #     end
-
-    #     it "should allow users to comment on ideas" do
-    #       assert has_content? "1 Comment"
-    #     end
-
-    #     it "should allow commenters to delete their comment" do
-    #       assert has_content? "1 Comment"
-    #       find(".comment .delete-comment").click
-    #       refute has_content? "1 Comments"
-    #     end
-
-    #     it "should allow commenters to edit their comment" do
-    #       find(".comment .edit-comment").click
-    #       find("#edit-comment")
-    #       fill_in :edit_comment_body, with: "Updated Comment!"
-    #       find(".save-updated-comment").click
-    #       comment = find(".comment")
-    #       assert comment.has_content? "Updated Comment!"
-    #     end
-    #   end
-    # end
+    it "allows a category to be deleted" do
+      visit categories_path
+      click_link "Destroy"
+      a = page.driver.browser.switch_to.alert.accept
+      assert has_content?("Category was successfully destroyed")
+    end
   end
 end
