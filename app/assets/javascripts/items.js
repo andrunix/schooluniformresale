4c0eda029f44caff0b950ded60e594a82337d5f2
Andrew Pierce <andrunix@gmail.com>
2015-09-15 16:26:11 -0400

    Work in progress on 'My Items'

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,3 +1,11 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$('#user_schools').change(function() {
+  $.get('items/toggle?' + $(this).val(), function(data, status) {
+      if (status === 'success') {
+        alert(data);
+      }
+    });
+  });
